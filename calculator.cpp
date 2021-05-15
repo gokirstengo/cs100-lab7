94c67e074f9444055eca3c45dc7eb19db8b1d489
Kevin20201 <kevin.yu.tu@gmail.com>
2021-05-15 14:12:41 -0700

    Added calculator.cpp and setup for factory.hpp

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "factory.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    Factory factory;
+    Base* tree = factory.parse(argv, argc);
+
+    if (tree) {
+        cout << tree->stringify() << " = " << tree->evaluate() << endl;
+    }
+
+    return 0;
+}
+
